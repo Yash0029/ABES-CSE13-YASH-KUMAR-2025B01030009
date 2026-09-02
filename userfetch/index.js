2a81427c7a2461b86b739b07ce73a702bb27c91f
Yash Kumar <yash@Yashs-MacBook-Air-3.local>
2026-09-02 15:33:30 +0530

    new

--- userfetch/index.js
+++ userfetch/index.js
@@ -1,25 +1,47 @@
-const conatainer=document.getElementById("container")
+const conatainer = document.getElementById("container");
 
-console.log(container);
+console.log(conatainer);
 
-const button=document.getElementById("btn");
+const button = document.getElementById("btn");
 console.log(button);
-const h2=document.getElementById('data');
-const loader=document.getElementById('loader');
-async function fetchData(){
-    try{
-        loader.innerHTML='fetching data..'
-    const serverData=await fetch('https://fakestoreapi.com/products')
-       const JSONData=await serverData.json();  
 
-       // console.log(JSONData[0].title)
-    h2.innerHTML=(JSONData[1].title);
-    }catch(e){
-        console.log("Error is:"+e)
-        loader.innerHTML='Error is'+e;
+const h2 = document.getElementById("data");
+const loader = document.getElementById("loader");
+
+async function fetchData() {
+    try {
+        loader.innerHTML = "fetching data..";
+
+        const serverData = await fetch("https://fakestoreapi.com/products");
+        const JSONData = await serverData.json();
+
+        // console.log(JSONData[0].title)
+        // h2.innerHTML = (JSONData[1].title);
+
+        let table = `<table border=2px>
+        ${
+            JSONData.map((ele) => (`
+                <tr>
+                    <td>
+                        <img src="${ele.image}" height="200px" width="200px">
+                    </td>
+                    <td>${ele.title}</td>
+                    <td>${ele.price}</td>
+                    <td>${ele.description}</td>
+                </tr>
+            `))
+        }
+        </table>`;
+
+        conatainer.innerHTML = table;
+
+    } catch (e) {
+        console.log("Error is:" + e);
+        loader.innerHTML = "Error is" + e;
     }
-    finally{
-   loader.innerHTML='';
+    finally {
+        loader.innerHTML = "";
     }
 }
-button.addEventListener("click",fetchData);
+
+button.addEventListener("click", fetchData);

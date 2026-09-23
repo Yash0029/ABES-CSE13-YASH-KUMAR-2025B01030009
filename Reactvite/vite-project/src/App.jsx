7192efe676c0f66252ec6e78315b4c45ea2c68d0
Yash Kumar <yash@Yashs-MacBook-Air-3.local>
2026-09-23 15:33:31 +0530

    react props

--- Reactvite/vite-project/src/App.jsx
+++ Reactvite/vite-project/src/App.jsx
@@ -1,65 +1,87 @@
-import car from './images/car.jpg'
+// import car from './images/car.jpg'
+// import UseProps from './component/UseProps'
+// import ICardGallery from './component/ICardGallery'
 
-function App() {
-  const student = {
-    name: "Yash",
-    course: "B.Tech CSE",
-    college: "ABES Engineering College",
-    rollNo: "250032010022",
-    year: "3rd Year",
-    section: "CSE-13"
-  }
+// function App() {
 
-  return (
-    <div style={{
-      width: "350px",
-      margin: "50px auto",
-      border: "2px solid white",
-      textAlign: "center",
-      backgroundColor:"yellow"
-    }}>
+//   const student = {
+//     name: "Yash",
+//     course: "B.Tech CSE",
+//     college: "ABES Engineering College",
+//     rollNo: "250032010022",
+//     year: "3rd Year",
+//     section: "CSE-13"
+//   }
 
-      <div style={{
-        backgroundColor: "Red",
-        padding: "10px",
-        color:"black"
-      }}>
-        <h2>ABES Engineering College</h2>
-        <p>Student Identity Card</p>
-      </div>
+//   // return (
+//   //   <div style={{
+//   //     width: "350px",
+//   //     margin: "50px auto",
+//   //     border: "2px solid white",
+//   //     textAlign: "center",
+//   //     backgroundColor:"yellow"
+//   //   }}>
 
-      <div style={{
-        padding: "20px",
-         backgroundColor:"white"
-      }}>
+//   //     <div style={{
+//   //       backgroundColor: "Red",
+//   //       padding: "10px",
+//   //       color:"black"
+//   //     }}>
+//   //       <h2>ABES Engineering College</h2>
+//   //       <p>Student Identity Card</p>
+//   //     </div>
 
-        <img
-          src={car}
-          alt="Student"
-          style={{
-            width: "120px",
-            height: "120px",
+//   //     <div style={{
+//   //       padding: "20px",
+//   //        backgroundColor:"white"
+//   //     }}>
+
+//   //       <img
+//   //         src={car}
+//   //         alt="Student"
+//   //         style={{
+//   //           width: "120px",
+//   //           height: "120px",
            
-          }}
-        />
+//   //         }}
+//   //       />
 
-        <h2 style={{
-          color:"black"
-        }}>{student.name}</h2>
+//   //       <h2 style={{
+//   //         color:"black"
+//   //       }}>{student.name}</h2>
 
-        <p><b>Course:</b> {student.course}</p>
-        <p><b>Roll No:</b> {student.rollNo}</p>
-        <p><b>Year:</b> {student.year}</p>
-        <p><b>Section:</b> {student.section}</p>
+//   //       <p><b>Course:</b> {student.course}</p>
+//   //       <p><b>Roll No:</b> {student.rollNo}</p>
+//   //       <p><b>Year:</b> {student.year}</p>
+//   //       <p><b>Section:</b> {student.section}</p>
 
-      </div>
+//   //     </div>
 
       
-        <p>{student.college}</p>
-      </div>
+//   //       <p>{student.college}</p>
+//   //     </div>
 
    
-  )
+//   // )
+// // }
+// return (
+//   <div>
+//     <ICardGallery />
+//   </div>
+// )
+// }
+
+// export default App
+
+
+import ICardGallery from "./component/ICardGallery";
+
+function App() {
+  return (
+    <div>
+      <ICardGallery />
+    </div>
+  );
 }
 
-export default App
+export default App;
